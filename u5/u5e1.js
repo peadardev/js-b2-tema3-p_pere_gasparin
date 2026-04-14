4d37995f1609ca74964a8bca57c25fcff184000b
Pere GT <peadar.dev@gmail.com>
2026-04-14 23:28:56 +0200

    ex. u5

--- u5/u5e1.js
+++ u5/u5e1.js
@@ -3,3 +3,23 @@
 // Enunciado disponible en u5e1.md / Enunciat disponible a u5e1.md
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
+class ClipboardApi {
+    clipboard;
+
+    constructor(clipboard) {
+        this.clipboard = clipboard ?? window.navigator.clipboard;
+    }
+
+    async copy(text) {
+        const resposta = await this.clipboard.writeText(text);
+        return resposta;
+    }
+
+    async read() {
+        const resposta = await this.clipboard.readText();
+        return resposta;
+    }
+}
+
+
+export { ClipboardApi };
